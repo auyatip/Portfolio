82ff703a5a52b62f4b3597a4a534bd7c7dcb4483
auyatip <atipthummakul@gmail.com>
2023-09-20 19:26:05 +0700

    second_commit

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -128,7 +128,7 @@ const Projects = () => {
         >
           <div
             data-aos="fade-up"
-            className=" flex items-center relative justify-center font-semibold  text-white rounded-3xl h-[250px] min-h-[250px] shadow-lg shadow-[#25ffc9] "
+            className=" flex items-center relative justify-center font-semibold  text-white rounded-3xl h-[250px] shadow-lg shadow-[#25ffc9] "
           >
             <img
               src={project3}
@@ -173,7 +173,7 @@ const Projects = () => {
         >
           <div
             data-aos="fade-up"
-            className=" flex items-center relative justify-center font-semibold  text-white rounded-3xl h-[250px] min-h-[250px] shadow-lg shadow-[#25ffc9]"
+            className=" flex items-center relative justify-center font-semibold  text-white rounded-3xl h-[250px] shadow-lg shadow-[#25ffc9]"
           >
             <img
               src={project2}
@@ -219,7 +219,7 @@ const Projects = () => {
         >
           <div
             data-aos="fade-up"
-            className=" flex items-center relative justify-center font-semibold  text-white rounded-3xl h-[250px] min-h-[250px] shadow-lg shadow-[#25ffc9] "
+            className=" flex items-center relative justify-center font-semibold  text-white rounded-3xl h-[250px] shadow-lg shadow-[#25ffc9] "
           >
             <img
               src={project4}
